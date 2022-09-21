dbae369cf50ab5c4b8982d2a18b367a8804d88b7
Saffet Gokcen Sen <saffetgokcensen@hotmail.com>
2022-09-21 22:17:30 +0300

    access to the passwd and passwd confirmation elements is defined

---
+++ javascript/passwd.js
@@ -0,0 +1,7 @@
+/* 
+used some of the information in the following link:
+https://www.w3schools.com/howto/howto_js_password_validation.asp 
+*/
+
+let passwd = document.getElementById("passwd");
+let passwd_confirm = document.getElementById("passwd_confirm");
