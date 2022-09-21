6c62cea0587d8e9755186173d5b10a7385f0b908
Saffet Gokcen Sen <saffetgokcensen@hotmail.com>
2022-09-21 22:41:12 +0300

    submit button is disabled if the passwords do not match

--- javascript/passwd.js
+++ javascript/passwd.js
@@ -5,3 +5,16 @@ https://www.w3schools.com/howto/howto_js_password_validation.asp
 
 let passwd = document.getElementById("passwd");
 let passwd_confirm = document.getElementById("passwd_confirm");
+let submit_button = document.getElementById("submit_button");
+
+function checkMatch() {
+    if (passwd.value !== passwd_confirm.value) {
+        submit_button.disabled = true;
+    }
+    else {
+        submit_button.disabled = false;
+    }
+}
+
+passwd.addEventListener('keyup', checkMatch);
+passwd_confirm.addEventListener('keyup', checkMatch);
