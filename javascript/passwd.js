6a99859cdd9c8f0819870e5ecd1ac63d8a0d5920
Saffet Gokcen Sen <saffetgokcensen@hotmail.com>
2022-09-27 00:48:30 +0300

    passwd mismatch warning shown

--- javascript/passwd.js
+++ javascript/passwd.js
@@ -6,17 +6,20 @@ https://www.w3schools.com/howto/howto_js_password_validation.asp
 let passwd = document.getElementById("passwd");
 let passwd_confirm = document.getElementById("passwd_confirm");
 let submit_button = document.getElementById("submit_button");
+let passwd_warning = document.getElementById("passwd-warning");
 
 function checkMatch() {
     if (passwd.value !== passwd_confirm.value) {
         submit_button.disabled = true;
         submit_button.classList.remove("active-button");
         submit_button.classList.add("disabled-button");
+        passwd_warning.classList.add("warning");
     }
     else {
         submit_button.disabled = false;
         submit_button.classList.remove("disabled-button");
         submit_button.classList.add("active-button");
+        passwd_warning.classList.remove("warning");
     }
 }
 
