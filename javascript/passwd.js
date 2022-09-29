0b27f7e3e14ab7bd877116ab159b9464b9280c91
Saffet Gokcen Sen <saffetgokcensen@hotmail.com>
2022-09-29 22:50:31 +0300

    paint red borders for the passwd fields for the case of passwd mismatch

--- javascript/passwd.js
+++ javascript/passwd.js
@@ -14,12 +14,16 @@ function checkMatch() {
         submit_button.classList.remove("active-button");
         submit_button.classList.add("disabled-button");
         passwd_warning.classList.add("warning");
+        passwd.classList.add("red-border");
+        passwd_confirm.classList.add("red-border");
     }
     else {
         submit_button.disabled = false;
         submit_button.classList.remove("disabled-button");
         submit_button.classList.add("active-button");
         passwd_warning.classList.remove("warning");
+        passwd.classList.remove("red-border");
+        passwd_confirm.classList.remove("red-border");
     }
 }
 
